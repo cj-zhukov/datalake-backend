96c3aef5a1a3c86107e61e8b218f3666f7399c03
Sergey Zhukov <szhukov@aligntech.com>
2025-11-19 11:53:41 +0300

    Fix: remove api from code

--- datalake-client/datalake_client/main.py
+++ datalake-client/datalake_client/main.py
@@ -9,18 +9,18 @@ from datafusion import SessionContext
 logging.basicConfig(level=logging.INFO)
 
 
-URL = "https://wg4w0o8cad.execute-api.eu-central-1.amazonaws.com/test"
+URL = "path-to-api"
 CHUNK_SIZE = 8192
-CHECK_INTERVAL = 5 # secs 
+CHECK_INTERVAL = 5  # secs
 MAX_RETRIES = 1000  # ~80 min CHECK_INTERVAL * MAX_RETRIES = max time waiting for file
+
 
 @dataclass
 class ObjectStoreClient:
     query: str = "select * from 's3://path-to-data/data/' limit 100"
-    path: str = "query" 
+    path: str = "query"
     method: str = "post"
     output: str = "/tmp/download.parquet"
-
 
     def __post_init__(self):
         """Post-initialization checks."""
@@ -30,7 +30,6 @@ class ObjectStoreClient:
             raise ValueError("Query must be provided")
         if self.method.lower() not in {"post"}:
             raise ValueError(f"Unsupported HTTP method: {self.method}")
-    
 
     def run(self) -> None:
         """Datalake-client main interface"""
@@ -43,21 +42,20 @@ class ObjectStoreClient:
                 self._wait_for_result(presigned_url)
             case _:
                 raise ValueError(f"Unsupported path: {self.path}")
-            
-            
+
     def _send_request(self, url: str) -> dict | str | None:
         """Sends a request and returns response or None."""
         payload = {"query": self.query}
         try:
             response = requests.post(url, json=payload)
-            response.raise_for_status() 
-            return response.json().get("result_parquet") # get url for parquet file
+            response.raise_for_status()
+            return response.json().get("result_parquet")  # get url for parquet file
         except requests.HTTPError as http_err:
-            logging.error(f"HTTP error: {http_err} | Status Code: {response.status_code}")
+            logging.error(
+                f"HTTP error: {http_err} | Status Code: {response.status_code}")
         except requests.RequestException as req_err:
             logging.error(f"Request error: {req_err}")
         return None
-
 
     def _wait_for_result(self, url: str, chunk_size=CHUNK_SIZE) -> None:
         """Polls until file is ready, then downloads and reads."""
@@ -69,7 +67,6 @@ class ObjectStoreClient:
             time.sleep(CHECK_INTERVAL)
             retries += 1
         raise TimeoutError("Timed out waiting for file to become available.")
-
 
     def _try_download(self, url: str) -> bool:
         """Try to download Parquet file once."""
@@ -100,10 +97,13 @@ class ObjectStoreClient:
 def main():
     parser = argparse.ArgumentParser(description="Datalake Client")
     parser.add_argument("-p", "--path", type=str, required=True, help="Path")
-    parser.add_argument("-q", "--query", type=str, required=True, help="SQL query")
-    parser.add_argument("-o", "--output", type=str, default="result.parquet", help="Output file path for download")
+    parser.add_argument("-q", "--query", type=str,
+                        required=True, help="SQL query")
+    parser.add_argument("-o", "--output", type=str,
+                        default="result.parquet", help="Output file path for download")
     args = parser.parse_args()
-    client = ObjectStoreClient(query=args.query, path=args.path, method="post", output=args.output)
+    client = ObjectStoreClient(
+        query=args.query, path=args.path, method="post", output=args.output)
     client.run()
 
 
